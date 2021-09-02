4b17b3ea94b7f1218cf62b630124ad1fd488a046
Fernanda González <fernandagonzalez@ufm.edu>
2021-09-01 21:21:30 -0600

    hola qué tal

--- app/main.py
+++ app/main.py
@@ -1,14 +1,27 @@
 from fastapi import FastAPI, Query
 from typing import List
 from typing import Optional
+import random
 
 app = FastAPI()
 
 
 @app.get("/ranking")
-def read_root(advertiser_campaigns: List[int], advertiser_campaigns_bids: List[int], maximum: Optional[int] = 10):
+def read_root(advertiser_campaigns: List[int], advertiser_campaigns_bids: List[float], maximum: Optional[int] = 10):
+    # Shuffle tuples list in case of repeated bids
+    shuffled = list(zip(advertiser_campaigns, advertiser_campaigns_bids))
+    random.shuffle(shuffled)
+
+    # Sort according bids
+    sorted_advertiser_campaigns = sorted(shuffled, key=lambda x: x[1])
     
-    return {"Hello": "World"}
+    # Keep only ids according maximum 
+    campaign_ids = []
+    for index, tuple in enumerate(sorted_advertiser_campaigns):
+        if index < maximum-1:
+            campaign_ids.append(tuple[0])
+
+    return campaign_ids
 
 
 
